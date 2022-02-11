f1168745555dabdde7880eb369a63b52c602ceb5
Dhruv Kapoor <kapoor_1992@hotmail.com>
2022-02-10 22:54:34 -0800

    core logic done on new metric

--- src/ml/data/metrics.py
+++ src/ml/data/metrics.py
@@ -9,10 +9,16 @@ def get_matches_dict(df_x, df_y):
         'D': [4, 0, 0]
     }
     
-    for i, row in df_x.iterrows():
+    df_x_copy = df_x.copy()
+    df_x_copy.reset_index(drop=True, inplace=True)
+    
+    df_y_copy = df_y.copy()
+    df_y_copy.reset_index(drop=True, inplace=True)
+
+    for i, row in df_x_copy.iterrows():
         for k, v in matches_dict.items():
             if row[f'ASIA - Admission_{k}'] == 1:
-                if df_y[i] == v[0]:
+                if df_y_copy[i] == v[0]:
                     v[1] += 1
                 else:
                     v[2] += 1
@@ -35,33 +41,85 @@ def display_matches_dict(df_x=None, df_y=None, matches_dict=None, only_rate=Fals
     
     return matches_dict
 
-def run_metrics(clf, train_x, train_y, test_x, test_y):
+def get_admission_to_correct_dict(df_x, df_y, predictions):
+    '''
+    Returns a dictionary where the admission ASIA score is mapped to the number of correct predictions.
+    '''
+    # List values are (<key_equivalent_in_target_variable>, <correct>, <incorrect>)
+    admission_to_correct_dict = {
+        'A': [1, 0, 0], 
+        'B': [2, 0, 0], 
+        'C': [3, 0, 0], 
+        'D': [4, 0, 0]
+    }
+    
+    df_x_copy = df_x.copy()
+    df_x_copy.reset_index(drop=True, inplace=True)
+    
+    df_y_copy = df_y.copy()
+    df_y_copy.reset_index(drop=True, inplace=True)
+
+    for i, row in df_x_copy.iterrows():
+        for k, v in admission_to_correct_dict.items():
+            if row[f'ASIA - Admission_{k}'] == 1:
+                if df_y_copy[i] == predictions[i]:
+                    v[1] += 1
+                else:
+                    v[2] += 1
+                break
+    
+    return admission_to_correct_dict
+
+def display_admission_to_correct_dict(df_x=None, df_y=None, predictions=None, admission_to_correct_dict=None, only_rate=False):
+    '''
+    Must provide one of df_x/df_y/predictions OR admission_to_correct_dict
+    '''
+    if admission_to_correct_dict is None:
+        admission_to_correct_dict = get_admission_to_correct_dict(df_x, df_y, predictions)
+
+    for k, v in admission_to_correct_dict.items():
+        if not only_rate:
+            print(f'Admission ASIA {k} Correct:      {v[1]}')
+            print(f'Admission ASIA {k} Wrong:        {v[2]}')
+        print(f'Admission ASIA {k} Correct Rate: {round(v[1] / (v[1] + v[2]), 4)}')
+    
+    return admission_to_correct_dict
+
+def run_metrics(clf, train_x, train_y, test_x, test_y, train_matches_dict, test_matches_dict):
     """
     Print ML classifier metrics on train and test
     """
     accuracyTrain = clf.score(train_x, train_y)
     print(f'Accuracy on train set: {accuracyTrain}')
 
-    train_prediction = clf.predict(train_x)
-    test_prediction = clf.predict(test_x)
+    train_predictions = clf.predict(train_x)
+    test_predictions = clf.predict(test_x)
     
     accuracy = clf.score(test_x, test_y)
     print(f'Accuracy on test set: {accuracy}')
 
-    precisionTrain = precision_score(train_y, train_prediction, average="macro")
+    precisionTrain = precision_score(train_y, train_predictions, average="macro")
     print(f'Precision on train set: {precisionTrain}')
     
-    precision = precision_score(test_y, test_prediction, average="macro")
+    precision = precision_score(test_y, test_predictions, average="macro")
     print(f'Precision on test set: {precision}')
 
-    recallTrain = recall_score(train_y, train_prediction, average="macro")
+    recallTrain = recall_score(train_y, train_predictions, average="macro")
     print(f'Recall on train set: {recallTrain}')
     
-    recall = recall_score(test_y, test_prediction, average="macro")
+    recall = recall_score(test_y, test_predictions, average="macro")
     print(f'Recall on test set: {recall}')
     
-    f1Train = f1_score(train_y, train_prediction, average="macro")
+    f1Train = f1_score(train_y, train_predictions, average="macro")
     print(f'F1-Score on train set: {f1Train}')
     
-    f1 = f1_score(test_y, test_prediction, average="macro")
+    f1 = f1_score(test_y, test_predictions, average="macro")
     print(f'F1-Score on test set: {f1}')
+
+    print('-----')
+    print('Admission ASIA to correct prediction results (train set):')
+    display_admission_to_correct_dict(train_x, train_y, train_predictions)
+    
+    print('-----')
+    print('Admission ASIA to correct prediction results (test set):')
+    display_admission_to_correct_dict(test_x, test_y, test_predictions)
--- src/ml/modelling/sklearn_model_runs.py
+++ src/ml/modelling/sklearn_model_runs.py
@@ -9,26 +9,28 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 
-def run_random_forest(train_x, train_y, test_x, test_y, num_trees):
+def run_random_forest(train_x, train_y, test_x, test_y, train_matches_dict, test_matches_dict, num_trees):
     clf = RandomForestClassifier(n_estimators=num_trees)
     clf.fit(train_x, train_y)
     
     clf.inference_features = list(test_x.columns.values)
 
+    print('-----')
     print('Random Forest')
-    metrics.run_metrics(clf, train_x, train_y, test_x, test_y)
+    metrics.run_metrics(clf, train_x, train_y, test_x, test_y, train_matches_dict, test_matches_dict)
 
     return clf
 
 # Best model yet.
-def run_elastic_net(train_x, train_y, test_x, test_y, penalty_type, iters, solver_name, l1_ratio_frac):
+def run_elastic_net(train_x, train_y, test_x, test_y, train_matches_dict, test_matches_dict, penalty_type, iters, solver_name, l1_ratio_frac):
     clf = LogisticRegression(penalty=penalty_type, max_iter=iters, solver=solver_name, l1_ratio=l1_ratio_frac)
     clf.fit(train_x, train_y)
 
     clf.inference_features = list(test_x.columns.values)
 
+    print('-----')
     print('Elastic Net')
-    metrics.run_metrics(clf, train_x, train_y, test_x, test_y)
+    metrics.run_metrics(clf, train_x, train_y, test_x, test_y, train_matches_dict, test_matches_dict)
 
     return clf
 
@@ -44,20 +46,26 @@ def main():
     df = dataset_loader.get_dataset_df('../data/csvs/f1_public.csv', '../data/csvs/translated_dataset.csv')
     train_x, train_y, test_x, test_y = dataset_loader.get_train_test_split(df)
 
+    print('-----')
     print('ASIA matches between admission and discharge in training set:')
     train_matches_dict = metrics.display_matches_dict(train_x, train_y)
+
+    print('-----')
     print('ASIA matches between admission and discharge in testing set:')
     test_matches_dict = metrics.display_matches_dict(test_x, test_y)
-    
+
+    print('-----')
     print(f'Total samples:          {train_y.shape[0] + test_y.shape[0]}') # 20,790
     print(f'Total training samples: {train_y.shape[0]}') # 18,737
     print(f'Total testing samples:  {test_y.shape[0]}') # 2,053
     print(f'Total ML features:      {train_x.shape[1]}') # 53
     print(f'Total "real" features:  {len(dataset_loader.get_practical_features(train_x))}') # 18
-    
+    print('-----')
+
     # Add model runs here.
     # clf = run_random_forest(train_x, train_y, test_x, test_y, num_trees=1000)
-    clf = run_elastic_net(train_x, train_y, test_x, test_y, penalty_type='elasticnet', iters=200, solver_name='saga', l1_ratio_frac=0.9)
+    clf = run_elastic_net(train_x, train_y, test_x, test_y, train_matches_dict, test_matches_dict, 
+                          penalty_type='elasticnet', iters=200, solver_name='saga', l1_ratio_frac=0.9)
 
     # Save model.
     with open('pickles/model.pkl', 'wb') as f:
