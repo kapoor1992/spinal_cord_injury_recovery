e1c3f12f1b0e2d60b748d283af710f161c351da1
Dhruv Kapoor <kapoor_1992@hotmail.com>
2022-03-13 14:17:11 -0700

    remove ios

--- src/mobile_app/SCI Recovery/Pods/Target Support Files/Pods-SCI Recovery-SCI RecoveryUITests/Pods-SCI Recovery-SCI RecoveryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SCI_Recovery_SCI_RecoveryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SCI_Recovery_SCI_RecoveryUITests
-@end
--- src/mobile_app/SCI Recovery/Pods/Target Support Files/Pods-SCI RecoveryTests/Pods-SCI RecoveryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SCI_RecoveryTests : NSObject
-@end
-@implementation PodsDummy_Pods_SCI_RecoveryTests
-@end
